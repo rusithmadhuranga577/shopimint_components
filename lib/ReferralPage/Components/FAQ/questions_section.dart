8f7e1dea4adac9f15444c4a12d46636a8874336d
asela-softmint <128887356+asela-softmint@users.noreply.github.com>
2023-05-12 16:26:25 +0530

    ..

--- lib/ReferralPage/Components/FAQ/questions_section.dart
+++ lib/ReferralPage/Components/FAQ/questions_section.dart
@@ -1,4 +1,3 @@
-import 'package:expandable/expandable.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:ontecso_components/ReferralPage/Components/FAQ/question.dart';
